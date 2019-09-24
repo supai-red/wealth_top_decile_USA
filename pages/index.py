c5a4629cdd66ad3c778cbfeee637ffef2c134124
supai-red <andrea@sedonaphilosophy.com>
2019-09-24 15:53:44 -0700

    Add app name and contact info

--- pages/index.py
+++ pages/index.py
@@ -10,17 +10,17 @@ from app import app
 """
 https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 
-Layout in Bootstrap is controlled using the grid system. The Bootstrap grid has 
+Layout in Bootstrap is controlled using the grid system. The Bootstrap grid has
 twelve columns.
 
-There are three main layout components in dash-bootstrap-components: Container, 
+There are three main layout components in dash-bootstrap-components: Container,
 Row, and Col.
 
 The layout of your app should be built as a series of rows of columns.
 
-We set md=4 indicating that on a 'medium' sized or larger screen each column 
-should take up a third of the width. Since we don't specify behaviour on 
-smaller size screens Bootstrap will allow the rows to wrap so as not to squash 
+We set md=4 indicating that on a 'medium' sized or larger screen each column
+should take up a third of the width. Since we don't specify behaviour on
+smaller size screens Bootstrap will allow the rows to wrap so as not to squash
 the content.
 """
 
@@ -28,10 +28,10 @@ column1 = dbc.Col(
     [
         dcc.Markdown(
             """
-        
-            ## Value Proposition
 
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
+            ## Value Proposition for Piketty Here
+
+            Andrea's description here.
 
             ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
 
@@ -39,7 +39,7 @@ column1 = dbc.Col(
 
             """
         ),
-        dcc.Link(dbc.Button('Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Learn more', color='light'), href='/predictions')
     ],
     md=4,
 )
